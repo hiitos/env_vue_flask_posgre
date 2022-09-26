2fdcedc209ecbf77e56e36909cfe2bb1f8c9c710
hitose kakazu <hkakazu@hitosenoMacBook-Air.local>
2022-09-26 23:42:40 +0900

    Dockerでvueとflaskの環境を構築した

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,43 @@
+<!-- HTMLを記述 -->
+<template>
+  <div>
+    <p>Home</p>
+    <button @click="getRandom">占う</button>
+    <p>Random number from backend: {{ randomNumber }}</p>
+    <h1 v-if="randomNumber%4==0">Awesome!!!</h1>
+    <h2 v-if="randomNumber%4==1">Good</h2>
+    <h2 v-if="randomNumber%4==2">Bad...</h2>
+    <h1 v-if="randomNumber%4==3">S〇〇ks!!!</h1>
+  </div>
+</template>
+
+<!-- JavaScriptを記述 -->
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      randomNum: 0
+    }
+  },
+  methods: {
+    getRandom () {
+      this.randomNum = this.getRandomNum()
+    },
+    getRandomNum () {
+      const path = 'http://localhost:9000/rand'
+      axios.get(path)
+        .then(response => {
+          this.randomNum = response.data.randomNum
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.getRandom()
+  }
+}
+</script>
